84bebbea4d3c2ed40c6884ec6884421e91edc2f0
shaikhjunaidx <shaikhjunaidx@gmail.com>
2024-08-20 23:40:48 -0400

    Add navigation to landing page on logo and title click

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,39 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
-import RecipesPage from './pages/RecipesPage'; 
-import RecipeDetails from './components/RecipeDetails'; 
-import './App.css'; // Make sure you import your App.css
+import RecipesPage from './pages/RecipesPage';
+import RecipeDetails from './components/RecipeDetails';
+import './App.css';
 
 function App() {
+  const navigate = useNavigate();
+
+  const handleLogoClick = () => {
+    navigate('/');
+  };
+
   return (
-    <div className="App"> {/* Main container for your app */}
-      <div className='logo-container'>
+    <div className="App">
+      <div className='logo-container' onClick={handleLogoClick} style={{ cursor: 'pointer' }}>
         <img src='/catCook.png' alt='Website Logo' />
         <h1>The Lazy Chef</h1>
       </div>
 
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<LandingPage />} /> 
-          <Route path="/:mealType" element={<RecipesPage/>} /> 
-          <Route path="/:mealType/:recipeId" element={<RecipeDetails />} />
-        </Routes>
-      </BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/:mealType" element={<RecipesPage />} />
+        <Route path="/:mealType/:recipeId" element={<RecipeDetails />} />
+      </Routes>
     </div>
   );
 }
 
-export default App;
+function AppWrapper() {
+  return (
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+}
+
+export default AppWrapper;
