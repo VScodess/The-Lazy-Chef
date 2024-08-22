b6ba672c99863d4e0831f0a9a319ed55c401f7e0
Vineet <singhvineet.y17@gmail.com>
2024-08-22 01:01:37 -0400

    Refactor recipe details

--- frontend/src/components/RecipeDetails.js
+++ frontend/src/components/RecipeDetails.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
-import './RecipeDetails.css';
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import "./RecipeDetails.css";
 
 const RecipeDetails = () => {
   const { mealType, recipeId } = useParams();
@@ -11,9 +11,11 @@ const RecipeDetails = () => {
   useEffect(() => {
     const fetchRecipe = async () => {
       try {
-        const response = await fetch(`http://localhost:8080/recipes/${mealType}/${recipeId}`);
+        const response = await fetch(
+          `http://localhost:8080/recipes/${mealType}/${recipeId}`
+        );
         if (!response.ok) {
-          throw new Error('Failed to fetch recipe');
+          throw new Error("Failed to fetch recipe");
         }
         const data = await response.json();
         setRecipe(data);
@@ -36,21 +38,43 @@ const RecipeDetails = () => {
   }
 
   return (
-    <div className="container">
-      <h1 className="title">{recipe.name}</h1>
+    <div className="rd_container">
+      <div className="title-container">
+        {/* <img
+          src={`data:image/jpeg;base64,${recipe.image}`}
+          alt="Recipe Image"
+          className="recipe-image"
+        /> */}
+        <h1 className="title">{recipe.name}</h1>
+      </div>
 
       <div className="section">
-        <h2 className="section-title">Ingredients</h2>
-        <ul className="list">
-          {recipe.ingredients.map((ingredient, index) => (
-            <li key={index} className="list-item">{ingredient}</li>
-          ))}
-        </ul>
+        <div className="ingredient-container">
+          <h2 className="section-title">Ingredients</h2>
+          <ol className="ing_list">
+            {recipe.ingredients.map((ingredient, index) => (
+              <li key={index} className="ing_list-item">
+                {ingredient}
+              </li>
+            ))}
+          </ol>
+        </div>
+        <img
+          src={`data:image/jpeg;base64,${recipe.image}`}
+          alt="Recipe Image"
+          className="recipe-image"
+        />
       </div>
 
       <div className="section">
         <h2 className="section-title">Instructions</h2>
-        <p className="instructions">{recipe.steps}</p>
+        <ol className="ins_list">
+          {recipe.steps.map((step, index) => (
+            <li key={index} className="ins_list-item">
+              {step}
+            </li>
+          ))}
+        </ol>
       </div>
     </div>
   );
