5fabb2b23cfc9887581e24b31826e9d2d354b2c5
shaikhjunaidx <shaikhjunaidx@gmail.com>
2024-08-25 14:05:19 -0400

    Added gitignore

--- backend/config/config.go
+++ backend/config/config.go
@@ -3,17 +3,25 @@ package config
 import (
 	"log"
 	"os"
+
+	"github.com/joho/godotenv"
 )
 
 type Config struct {
-	MongoURI     string
-	DatabaseName string
+	MongoURI       string
+	DatabaseName   string
 	CollectionName string
-	ServerPort   string
+	ServerPort     string
 }
 
 // LoadConfig loads configuration from environment variables or defaults
 func LoadConfig() *Config {
+	// Load the .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Printf("Error loading .env file, using environment variables: %v", err)
+	}
+
 	config := &Config{
 		MongoURI:       getEnv("MONGO_URI", "mongodb://localhost:27017"),
 		DatabaseName:   getEnv("DB_NAME", "lazy_chef"),
